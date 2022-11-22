43671abc9bbd22b0e4c91eaa48b7439223b3e14a
RyFax <ryfax@git>
2022-11-22 15:42:47 +0100

    Initial push

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import mqtt "github.com/eclipse/paho.mqtt.golang"
+
+func main() {
+
+}
+
+func createClientOptions(brokerURI string, clientId string) *mqtt.ClientOptions {
+	return nil
+}
