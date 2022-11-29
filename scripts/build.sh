5fee7f5199d4fd76d83f76f3e2845c92bda7a69c
RyFax <ryfax@git>
2022-11-29 17:59:47 +0100

    script build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,28 @@
-# Build tout le tralala
-# Copier les fichiers de configs par défaut
+#!/bin/sh
+
+# Settings
+WORKDIR="$HOME/Documents/Projets/TP_airport/"
+INSTALL_PATH="$HOME/airport"
+AIRPORT="NTE"
+
+# Change directory to the workdir
+cd "$WORKDIR"
+
+# Clean & Build
+go clean ./...
+rm -rd "$INSTALL_PATH"
+go install ./...
+
+# Making dir
+mkdir "$INSTALL_PATH/"
+mkdir "$INSTALL_PATH/$AIRPORT/"
+mkdir "$INSTALL_PATH/$AIRPORT/sensors"
+mkdir "$INSTALL_PATH/$AIRPORT/sensors/pressure"
+mkdir "$INSTALL_PATH/$AIRPORT/sensors/temperature"
+mkdir "$INSTALL_PATH/$AIRPORT/sensors/wind"
+mkdir "$INSTALL_PATH/$AIRPORT/pubs"
+
+# Move bins
+mv "$GOPATH/bin/pressure" "$INSTALL_PATH/$AIRPORT/sensors/pressure/"
+mv "$GOPATH/bin/temperature" "$INSTALL_PATH/$AIRPORT/sensors/temperature/"
+mv "$GOPATH/bin/wind" "$INSTALL_PATH/$AIRPORT/sensors/wind/"
