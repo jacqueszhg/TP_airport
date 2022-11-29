308733158d118e8f5c3420c9899db2b0bd011313
RyFax <ryfax@git>
2022-11-29 16:39:06 +0100

    Adding client.go

---
+++ cmd/pubs/temperature/go.mod
@@ -0,0 +1,7 @@
+module Airport/cmd/pubs/temperature
+
+go 1.19
+
+require (
+	github.com/eclipse/paho.mqtt.golang v1.4.2 // indirect
+)
--- internal/pkg/mqtt/client.go
+++ internal/pkg/mqtt/client.go
@@ -9,33 +9,34 @@ import (
 
 func createClientOptions(brokerURI string, clientId string) *mqtt.ClientOptions {
 	opts := mqtt.NewClientOptions()
-	// AddBroker adds a broker URI to the list of brokers to be used.
-	// The format should be "scheme://host:port"
 	opts.AddBroker(brokerURI)
-	// opts.SetUsername(user)
-	// opts.SetPassword(password)
 	opts.SetClientID(clientId)
+
 	return opts
 }
 
 func Connect(brokerURI string, clientId string) mqtt.Client {
 	fmt.Println("Trying to connect (" + brokerURI + ", " + clientId + ")...")
+
 	opts := createClientOptions(brokerURI, clientId)
 	client := mqtt.NewClient(opts)
 	token := client.Connect()
+
 	for !token.WaitTimeout(3 * time.Second) {
 	}
+
 	if err := token.Error(); err != nil {
 		log.Fatal(err)
 	}
-	return client
 
+	return client
 }
 
+// MessageSensorPublisher TODO faire un parse / unparse (pour pub et sub) qui va automatiquement récupérer les données
 type MessageSensorPublisher struct {
-	id         int
-	idAirport  int
-	timestamp  string
-	value      float64
-	sensorType string
+	sensorId    int
+	sensorType  string
+	airportCode int
+	timestamp   string
+	value       float64
 }
