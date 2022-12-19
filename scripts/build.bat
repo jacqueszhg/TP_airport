15fdb087ca5e24dbb1d0b1b1384ad7c36518505b
Lucas SAVARY <lucas.savary@sfr.fr>
2022-12-19 22:10:22 +0100

    .bat

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,29 +1,32 @@
 @echo off
 
 REM Settings
-SET WORKDIR="%HOME%/Documents/Projets/TP_airport/"
-SET INSTALL_PATH="%HOME%/airport"
+SET WORKDIR="C:\Users\lucas\GolandProjects\TP_airport"
+SET INSTALL_PATH="%homedrive%%homepath%\airport"
 SET AIRPORT="NTE"
 
 REM Change directory to the workdir
 cd %WORKDIR%
 
 REM Clean & Build
-go clean ./...
+go clean .\...
 rmdir /S /Q "%INSTALL_PATH%"
-go install ./...
+go install .\...
 
 REM Making dir
 mkdir "%INSTALL_PATH%"
-mkdir "%INSTALL_PATH%/%AIRPORT%"
-mkdir "%INSTALL_PATH%/%AIRPORT%/sensors"
-mkdir "%INSTALL_PATH%/%AIRPORT%/sensors/pressure"
-mkdir "%INSTALL_PATH%/%AIRPORT%/sensors/temperature"
-mkdir "%INSTALL_PATH%/%AIRPORT%/sensors/wind"
-mkdir "%INSTALL_PATH%/%AIRPORT%/pubs"
+mkdir "%INSTALL_PATH%\%AIRPORT%"
+mkdir "%INSTALL_PATH%\%AIRPORT%\sensors"
+mkdir "%INSTALL_PATH%\%AIRPORT%\sensors\pressure"
+mkdir "%INSTALL_PATH%\%AIRPORT%\sensors\temperature"
+mkdir "%INSTALL_PATH%\%AIRPORT%\sensors\wind"
 
 REM Move bins
-mv "%GOPATH%/bin/pressure" "%INSTALL_PATH%/%AIRPORT%/sensors/pressure/"
-mv "%GOPATH%/bin/temperature" "%INSTALL_PATH%/%AIRPORT%/sensors/temperature/"
-mv "%GOPATH%/bin/wind" "%INSTALL_PATH%/%AIRPORT%/sensors/wind/"
+move "%GOPATH%\bin\pressure.exe" "%INSTALL_PATH%\%AIRPORT%\sensors\pressure\"
+move "%GOPATH%\bin\temperature.exe" "%INSTALL_PATH%\%AIRPORT%\sensors\temperature\"
+move "%GOPATH%\bin\wind.exe" "%INSTALL_PATH%\%AIRPORT%\sensors\wind\"
 
+REM Move configs
+copy "%WORKDIR%\configs\pubs\pressure\config.yml" "%INSTALL_PATH%\%AIRPORT%\sensors\pressure\"
+copy "%WORKDIR%\configs\pubs\temperature\config.yml" "%INSTALL_PATH%\%AIRPORT%\sensors\temperature\"
+copy "%WORKDIR%\configs\pubs\wind\config.yml" "%INSTALL_PATH%\%AIRPORT%\sensors\wind\"
