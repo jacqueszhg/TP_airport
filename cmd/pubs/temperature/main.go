549885848ca79a3ab1b363589e73e1733be63444
ZHENG Jacques <jacqueszheng22@gmail.com>
2022-12-20 23:42:28 +0100

    [Tsk] random temperature with location, saison and time + add main.go for register data in a logfile extension ".csv"

--- cmd/pubs/temperature/main.go
+++ cmd/pubs/temperature/main.go
@@ -5,9 +5,32 @@ import (
 	mqttConfig "Airport/internal/pkg/mqtt"
 	"encoding/json"
 	"fmt"
+	"math"
+	_ "math"
+	"math/rand"
 	"strconv"
 	"time"
 )
+
+var SAISON = map[string]float64{
+	"SUMMER": 30.0,
+	"SPRING": 20.0,
+	"AUTUMN": 10.0,
+	"WINTER": 0.0,
+}
+
+var LOCATION = map[string]float64{
+	"EQUATOR":     10.0,
+	"CONTINENTAL": 5.0,
+	"NORTH_POLE":  2.0,
+	"SOUTH_POLE":  2.0,
+}
+
+func simulateTemp(saison float64, location float64, time time.Time, oldLocation float64, oldTime float64) (float64, float64, float64) {
+	locationTemp := oldLocation + rand.Float64()*(location-oldLocation)
+	timeTemp := oldTime + rand.Float64()*((math.Sin(float64(time.Hour())/math.Pi)*location)-oldTime)
+	return saison + locationTemp - timeTemp, locationTemp, timeTemp
+}
 
 func main() {
 	configPub := config.GetSensorConfig("./config.yml")
@@ -21,29 +44,32 @@ func main() {
 
 	if err == nil {
 		fmt.Println(urlBroker)
-		msg := mqttConfig.MessageSensorPublisher{
-			SensorId:    sensorId,
-			SensorType:  "temperature",
-			AirportCode: sensor.Airport,
-			Timestamp:   time.Now().String(),
-			Value:       12.1,
-		}
-
-		bytesMsg, err := json.Marshal(msg)
-
-		if err != nil {
-			fmt.Println("Can't serislize", msg)
-		}
 
 		client := mqttConfig.Connect(urlBroker, sensor.Id) //TODO
-
+		currentTime := time.Now()
+		oldLocation := 0.0
+		oldTime := 0.0
+		currentTemp := 0.0
 		// Infinit loop for publish each "frenquency" secondes
 		for {
+			currentTemp, oldLocation, oldTime = simulateTemp(SAISON["SUMMER"], LOCATION["EQUATOR"], currentTime, oldLocation, oldTime)
+			msg := mqttConfig.MessageSensorPublisher{
+				SensorId:    sensorId,
+				SensorType:  "temperature",
+				AirportCode: sensor.Airport,
+				Timestamp:   time.Now().String(),
+				Value:       currentTemp,
+			}
+
+			bytesMsg, err := json.Marshal(msg)
+
+			if err != nil {
+				fmt.Println("Can't serislize", msg)
+			}
 			token := client.Publish("airport/temperature", byte(QOSLevel), true, bytesMsg)
 			token.Wait()
 			time.Sleep(time.Duration(frequency) * time.Second)
+			currentTime.Add(time.Duration(frequency) * (time.Second * 3600))
 		}
 	}
-
-	//TODO finir le capteur température
 }
