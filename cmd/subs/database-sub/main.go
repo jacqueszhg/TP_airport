91cf69bfeb455ecf0ca04bdb1dd1f836f867dc08
RyFax <ryfax@git>
2022-12-27 18:38:50 +0100

    First implementation

--- cmd/subs/database-sub/main.go
+++ cmd/subs/database-sub/main.go
@@ -1,34 +1,40 @@
 package main
 
 import (
-	mqttConfig "Airport/internal/pkg/mqtt"
-	"encoding/json"
-	"fmt"
-	mqtt "github.com/eclipse/paho.mqtt.golang"
-	"sync"
+	"context"
+	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
+	"github.com/influxdata/influxdb-client-go/v2/api/write"
+	"log"
+	"time"
 )
 
+/*
+ * SUB 1
+ */
+
 func main() {
-	wg := sync.WaitGroup{}
-	wg.Add(2)
+	token := "IJERnyHz_xzjSMxSRh2lL1OO7IxXhBXj-0UFf3V2FOguLu-lINu_st8o4swU_005YJL8vD7oNAq24F8QWnZm3Q=="
+	url := "https://europe-west1-1.gcp.cloud2.influxdata.com"
+	client := influxdb2.NewClient(url, token)
 
-	client := mqttConfig.Connect("tcp://localhost:1883", "sub")
-	client.Subscribe("airport/temperature", 1, myHandler)
-	client.Subscribe("airport/pressure", 1, myHandler)
+	org := "yt.ryfax@gmail.com"
+	bucket := "Tests"
+	writeAPI := client.WriteAPIBlocking(org, bucket)
 
-	fmt.Printf("finish")
-	wg.Wait()
-}
+	for value := 0; value < 5; value++ {
+		tags := map[string]string{
+			"city": "NTE",
+		}
+		
+		fields := map[string]interface{}{
+			"value": value,
+		}
+		point := write.NewPoint("wind", tags, fields, time.Now())
 
-func myHandler(client mqtt.Client, message mqtt.Message) {
-	// TODO : register in database
-	var r mqttConfig.MessageSensorPublisher
-	err := json.Unmarshal(message.Payload(), &r)
-	if err != nil {
-		fmt.Println("Can't deserislize", message.Payload())
+		time.Sleep(1 * time.Second) // separate points by 1 second
+
+		if err := writeAPI.WritePoint(context.Background(), point); err != nil {
+			log.Fatal(err)
+		}
 	}
-
-	fmt.Println(r)
-	fmt.Println("\n")
-
 }
--- go.mod
+++ go.mod
@@ -23,7 +23,13 @@ require (
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.11.1 // indirect
 	github.com/goccy/go-json v0.10.0 // indirect
+	github.com/deepmap/oapi-codegen v1.8.2 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/influxdata/influxdb-client-go/v2 v2.12.1 // indirect
+	github.com/influxdata/line-protocol v0.0.0-20200327222509-2487e7298839 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/net v0.0.0-20210119194325-5f4716e94777 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
