0435626688b299bb76fb5315edacb63a759c9ce8
ZHENG Jacques <jacqueszheng22@gmail.com>
2023-01-09 19:11:58 +0100

    copy config sub into installpath

--- scripts/build.bat
+++ scripts/build.bat
@@ -4,9 +4,9 @@ REM Settings
 SET FILEPATH=%~dp0
 SET WORKDIR="%FILEPATH:~0,-9%"
 REM Set the path of your project installation
-SET INSTALL_PATH="C:\Users\lucas\airport"
+SET INSTALL_PATH="C:\Users\jacqu\Desktop\airport"
 REM Set the airport IATA code
-SET AIRPORT="NTE"
+SET AIRPORT="LIEA"
 
 REM Change directory to the workdir
 cd %WORKDIR%
@@ -42,5 +42,7 @@ REM Move configs
 copy "%WORKDIR%\configs\pubs\pressure\config.yml" "%INSTALL_PATH%\%AIRPORT%\sensors\pressure\"
 copy "%WORKDIR%\configs\pubs\temperature\config.yml" "%INSTALL_PATH%\%AIRPORT%\sensors\temperature\"
 copy "%WORKDIR%\configs\pubs\wind\config.yml" "%INSTALL_PATH%\%AIRPORT%\sensors\wind\"
+copy "%WORKDIR%\configs\subs\database\config.yml" "%INSTALL_PATH%\sub\databaseConfig.yml"
+copy "%WORKDIR%\configs\subs\log\config.yml" "%INSTALL_PATH%\sub\logConfig.yml"
 copy "%WORKDIR%\scripts\runservices.bat" "%INSTALL_PATH%\"
 copy "%WORKDIR%\scripts\runsensors.bat" "%INSTALL_PATH%\%AIRPORT%\"
